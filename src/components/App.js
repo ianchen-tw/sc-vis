95fc81d00e5c87238b328d35ffee996fda047d46
Ian Chen <ianre657@gmail.com>
2021-05-07 19:08:50 +0800

    init with tailwind

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Visualize from "./Visualize"
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container mx-auto px-4">
+        <Visualize />
+      </div>
+    )
+  }
+}
+
+export default App
